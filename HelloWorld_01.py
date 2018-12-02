7473b8da1b3a5f3b3c4f68511997b939833f4aae
SuzuTakahiro <hw12a056@oecu.jp>
2018-12-02 15:05:31 +0900

    HelloWorld_01.py

---
+++ HelloWorld_01.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+if __name__ == '__main__':
+    app  = Tk()
+    #サイズを指定
+    app.geometry("400x300")
+    #タイトルを指定
+    app.title("Hello World Program")
+    #mainloopで画面を起こす
+    app.mainloop()
